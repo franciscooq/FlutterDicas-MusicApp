ceb293c2bcba68a2dd83323ff395942b26547055
franciscooq <franciscooq@yahoo.com.br>
2023-05-14 21:55:04 -0300

    Include Assets and define initial configs

---
+++ music_app/lib/core/app/music_app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MusicAppColors {
+  //construtor privado
+  MusicAppColors._();
+
+  //Black-color
+  static Color get primatyColor => const Color(0XFF000000);
+  //White-color
+  static Color get secondaryColor => const Color(0XFFFFFFFF);
+  //Green-color
+  static Color get tertiaryColor => const Color(0XFF779933);
+  //Red-color
+  static Color get errorColor => const Color(0XFFdc3545);
+}
